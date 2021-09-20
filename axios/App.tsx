453a3e8d359a581db8683aea667b5e04f28893f8
JwAndroid <cjd9408er@naver.com>
2021-09-20 12:51:09 +0900

    Update axios , annotations

--- axios/App.tsx
+++ axios/App.tsx
@@ -1,17 +1,9 @@
-import React, { memo, useCallback, useEffect } from 'react';
-import {
-  Alert,
-  Platform,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import React, { memo, useCallback } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import * as ImagePicker from 'expo-image-picker';
 
 import axios from 'axios';
-import { SERVER_URL } from './src/apiKey';
+// import { SERVER_URL } from './src/apiKey';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,76 +12,34 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#fff',
   },
-  text: {
-    fontSize: 24,
-    backgroundColor: 'cornflowerblue',
-    paddingHorizontal: 30,
-    paddingVertical: 15,
-    borderRadius: 8,
-    color: '#fff',
-  },
 });
 
 const App = () => {
-  useEffect(() => {
-    (async () => {
-      if (Platform.OS !== 'web') {
-        const { status } =
-          await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-          Alert.alert(
-            'Photo Permission',
-            'Please turn on the camera permission.'
-          );
-        }
-      }
-    })();
+  const onPressPost = useCallback(async () => {
+    await axios.post(
+      'https://my-json-server.typicode.com/typicode/demo/profile',
+      { id: 'jwid' }
+    );
   }, []);
 
-  // 이부분 다시
-  const uploadImage = useCallback(async (imageUri: string) => {
-    // const blob = new Blob([imageUri], { type: 'text/plain' });
-    // const getFiles = new File([blob], 'foo.txt', { type: 'text/plain' });
-    // console.log(`imageUri: ${imageUri}`);
-    // console.log('POST RESPONSE: $ ' + JSON.stringify(getFiles));
+  const onPressGet = useCallback(async () => {
+    const { data } = await axios.get(
+      'https://my-json-server.typicode.com/typicode/demo/profile'
+    );
 
-    try {
-      const formData = new FormData();
-      formData.append('file', imageUri);
-      // formData.append(
-      //   'file',
-      //   new Blob([JSON.stringify(imageUri)], { type: 'multipart/form-data' })
-      // );
-
-      const { data } = await axios.post(SERVER_URL, {
-        data: formData,
-        headers: { 'content-type': 'multipart/form-data' },
-      });
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
+    console.log(data);
   }, []);
-
-  const onPress = useCallback(async () => {
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
-
-    if (!result.cancelled && uploadImage) {
-      uploadImage(result.uri);
-    }
-  }, [uploadImage]);
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <Pressable onPress={onPress}>
-        <Text style={styles.text}>GO ALBUM</Text>
+      <Pressable>
+        <Text onPress={onPressPost}>Axios POST</Text>
+      </Pressable>
+
+      <Pressable>
+        <Text onPress={onPressGet}>Axios GET</Text>
       </Pressable>
     </View>
   );
