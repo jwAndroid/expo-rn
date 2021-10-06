90b66d152f91614ae12b81995accd28d2c824268
jwAndroid <cjd9408er@naver.com>
2021-10-06 12:10:09 +0900

    Update get Image-size with URL

--- image/image-size/App.tsx
+++ image/image-size/App.tsx
@@ -1,6 +1,7 @@
-import React, { memo } from 'react';
-import styled from '@emotion/native';
+import React, { memo, useCallback } from 'react';
+import { Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import styled from '@emotion/native';
 
 const Container = styled.View({
   flex: 1,
@@ -9,10 +10,26 @@ const Container = styled.View({
   justifyContent: 'center',
 });
 
+const StyledText = styled.Text({
+  fontSize: 18,
+  color: '#303030',
+});
+
+const IMAGE_URI =
+  'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko';
+
 const App = () => {
+  const onPress = useCallback(() => {
+    Image.getSize(IMAGE_URI, (width, height) => {
+      console.log({ width, height });
+    });
+  }, []);
+
   return (
     <Container>
       <StatusBar style="auto" />
+
+      <StyledText onPress={onPress}>getSize</StyledText>
     </Container>
   );
 };
