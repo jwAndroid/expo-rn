f6fe6d122f36204b2ea50bb7c74442b46843ba06
JwAndroid <cjd9408er@naver.com>
2021-10-10 20:41:10 +0900

    Update memo

--- hooks/App.tsx
+++ hooks/App.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useState, useMemo, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import styled from '@emotion/native';
 
@@ -9,10 +9,63 @@ const Container = styled.View({
   justifyContent: 'center',
 });
 
+const StyledButton = styled.Pressable({
+  paddingHorizontal: 50,
+  paddingVertical: 10,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#303030',
+  borderRadius: 8,
+  marginTop: 20,
+});
+
+const StyledText = styled.Text({
+  fontSize: 16,
+  color: '#fff',
+});
+
+const StyledImage = styled.Image({
+  width: '100%',
+  height: '50%',
+});
+
+const IMAGE_URL = 'https://pbs.twimg.com/media/ERcXkipUEAAzu-q.jpg';
+
+const PLACE_HOLDER =
+  'https://nkdi.kr/wp-content/themes/fox/images/placeholder.jpg';
+
 const App = () => {
+  const [image, setImage] = useState(IMAGE_URL);
+
+  useEffect(() => {
+    setImage(IMAGE_URL);
+  }, []);
+
+  const source = useMemo(() => {
+    return { uri: image };
+  }, [image]);
+
+  const onPress = useCallback(() => {
+    setImage(IMAGE_URL);
+  }, []);
+
+  const onPressPlaceHolder = useCallback(() => {
+    setImage(PLACE_HOLDER);
+  }, []);
+
   return (
     <Container>
       <StatusBar style="auto" />
+
+      <StyledImage source={source} />
+
+      <StyledButton onPress={onPressPlaceHolder}>
+        <StyledText>Place holder</StyledText>
+      </StyledButton>
+
+      <StyledButton onPress={onPress}>
+        <StyledText>Image</StyledText>
+      </StyledButton>
     </Container>
   );
 };
