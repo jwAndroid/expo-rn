5a3ece67f2d61b181c528091755d1d987801f09d
jwAndroid <cjd9408er@naver.com>
2021-10-13 11:55:44 +0900

    Ready for Flatlist

---
+++ flatlist/App.tsx
@@ -0,0 +1,20 @@
+import React, { memo } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import styled from '@emotion/native';
+
+const Container = styled.View({
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const App = () => {
+  return (
+    <Container>
+      <StatusBar style="auto" />
+    </Container>
+  );
+};
+
+export default memo(App);
