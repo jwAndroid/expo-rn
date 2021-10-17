7883c1c1fc869ef6d208ea9ab00fc9fc7185e2de
JwAndroid <cjd9408er@naver.com>
2021-10-17 18:42:59 +0900

    Update animate flatlist

--- flatlist/flatlist-animated-api/App.tsx
+++ flatlist/flatlist-animated-api/App.tsx
@@ -1,30 +1,96 @@
+import styled from '@emotion/native';
 import React, { memo } from 'react';
 import {
   StatusBar,
-  FlatList,
-  Image,
   Animated,
-  Text,
   View,
   Dimensions,
   StyleSheet,
 } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import { data } from './src/data';
+
+const { width } = Dimensions.get('screen');
+const imageW = width * 0.7;
+const imageH = imageW * 1.54;
+
+const Container = styled.View({
+  flex: 1,
+});
+
+const FlatListContainer = styled.View({
+  width,
+  justifyContent: 'center',
+  alignItems: 'center',
+  shadowColor: '#000',
+  shadowOpacity: 1,
+  shadowOffset: {
+    width: 0,
+    height: 0,
   },
+  shadowRadius: 20,
+});
+
+const FlatImage = styled.Image({
+  width: imageW,
+  height: imageH,
+  resizeMode: 'cover',
+  borderRadius: 16,
 });
 
 const App = () => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
   return (
-    <View style={styles.container}>
+    <Container>
       <StatusBar hidden />
-      <Text>FlatList Ready</Text>
-    </View>
+
+      <View style={StyleSheet.absoluteFillObject}>
+        {data.map((image, index) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width,
+          ];
+
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0, 1, 0],
+          });
+
+          return (
+            <Animated.Image
+              key={`image-${index}`}
+              source={{ uri: image }}
+              style={[StyleSheet.absoluteFillObject, { opacity }]}
+              blurRadius={20}
+            />
+          );
+        })}
+      </View>
+
+      <Animated.FlatList
+        data={data}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: { contentOffset: { x: scrollX } },
+            },
+          ],
+          { useNativeDriver: true }
+        )}
+        keyExtractor={(_, index) => index.toString()}
+        horizontal
+        pagingEnabled
+        renderItem={({ item }) => {
+          return (
+            <FlatListContainer>
+              <FlatImage source={{ uri: item }} />
+            </FlatListContainer>
+          );
+        }}
+      />
+    </Container>
   );
 };
 
---
+++ flatlist/flatlist-animated-api/src/data.ts
@@ -0,0 +1,8 @@
+export const data = [
+  'https://www.enjpg.com/img/2020/iron-man-7.jpg',
+  'https://wallpapercave.com/wp/wp9039511.jpg',
+  'https://www.enjpg.com/img/2020/iron-man-5.jpg',
+  'https://wallpaperbat.com/img/172620-iron-man-infinity-stones-armor-wallpaper-wallpaper.jpg',
+  'https://i.pinimg.com/originals/09/05/66/0905667728530c9fba2144e7ac6aefa7.jpg',
+  'https://my4kwallpapers.com/wp-content/uploads/2021/02/Iron-Man-4k-Wallpaper-For-Phone.png',
+];
