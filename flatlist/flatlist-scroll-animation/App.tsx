34ea4d3ee0ab9f6b4a61af98f6b17596e207111b
JwAndroid <cjd9408er@naver.com>
2021-10-17 19:36:08 +0900

    Update api

--- flatlist/flatlist-scroll-animation/App.tsx
+++ flatlist/flatlist-scroll-animation/App.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/native';
-import React, { memo } from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import {
   StatusBar,
   FlatList,
@@ -21,13 +21,40 @@ const { width, height } = Dimensions.get('screen');
 
 const Container = styled.View({
   flex: 1,
-  backgroundColor: '#fff',
+  backgroundColor: '#303030',
+});
+
+const Loading = styled.Text({
+  fontSize: 18,
 });
 
 const App = () => {
+  const [images, setImages] = useState(null);
+
+  const fetchImagesFromPexels = useCallback(async () => {
+    const data = await fetch(API_URL, { headers: { Authorization: API_KEY } });
+    const { photos } = await data.json();
+
+    return photos;
+  }, []);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const images = await fetchImagesFromPexels();
+      console.log(images);
+    };
+
+    fetchImages();
+  });
+
+  if (!images) {
+    return null;
+  }
+
   return (
     <Container>
       <StatusBar hidden />
+      <Loading>Loading...</Loading>
     </Container>
   );
 };
