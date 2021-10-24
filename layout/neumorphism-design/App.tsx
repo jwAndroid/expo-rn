fdc5b2f89f3f1e0f503b88169a6d756d8759fb37
jwandroid <cjd9408er@naver.com>
2021-10-24 14:26:37 +0900

    Ready for the ui design practice with ant

---
+++ layout/neumorphism-design/App.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default memo(App);
