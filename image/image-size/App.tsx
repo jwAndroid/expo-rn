aa2a7c3cb19f02ee90d610f48289c27baf860d58
choi_ji_woung <cjd9408er@naver.com>
2021-10-28 18:11:10 +0900

    Update annotation , get image size

--- image/image-size/App.tsx
+++ image/image-size/App.tsx
@@ -1,6 +1,7 @@
 import React, { memo, useCallback } from 'react';
 import { Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { manipulateAsync } from 'expo-image-manipulator';
 import styled from '@emotion/native';
 
 const Container = styled.View({
@@ -19,17 +20,23 @@ const IMAGE_URI =
   'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko';
 
 const App = () => {
-  const onPress = useCallback(() => {
-    Image.getSize(IMAGE_URI, (width, height) => {
-      console.log({ width, height });
-    });
+  const onPressManipulate = useCallback(async () => {
+    const result = await manipulateAsync(IMAGE_URI);
+
+    console.log(result);
   }, []);
+
+  // const onPressImage = useCallback(() => {
+  //   Image.getSize(IMAGE_URI, (width, height) => {
+  //     console.log({ width, height });
+  //   });
+  // }, []);
 
   return (
     <Container>
       <StatusBar style="auto" />
 
-      <StyledText onPress={onPress}>getSize</StyledText>
+      <StyledText onPress={onPressManipulate}>getSize</StyledText>
     </Container>
   );
 };
