a639d7ad2dba8a4212e138ea746f3f9163a470f3
choi_ji_woung <cjd9408er@naver.com>
2021-11-26 16:51:05 +0900

    캐러셀 업데이트

--- image/carousel/App.tsx
+++ image/carousel/App.tsx
@@ -1,21 +1,40 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import Carousel from 'react-native-snap-carousel';
 import styled from '@emotion/native';
-
-// https://www.npmjs.com/package/react-native-snap-carousel
-// https://docs.expo.dev/versions/latest/sdk/view-pager/
 
 const Container = styled.View({
   flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
+  backgroundColor: '#303030',
+});
+
+const StyledImage = styled.Image({
+  flex: 1,
 });
 
 const App = () => {
+  const [images, setImages] = useState([
+    'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko',
+    'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko',
+    'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko',
+    'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko',
+  ]);
+
+  const renderItem = useCallback(({ item }) => {
+    return <StyledImage source={{ uri: item }} resizeMode="contain" />;
+  }, []);
+
   return (
     <Container>
       <StatusBar style="auto" />
+
+      <Carousel
+        sliderWidth={200}
+        itemWidth={200}
+        firstItem={1}
+        data={images}
+        renderItem={renderItem}
+      />
     </Container>
   );
 };
