97ea6e99dc9a4997a5f1d818e1776da211d7ba2a
choi_ji_woung <cjd9408er@naver.com>
2021-11-29 11:00:52 +0900

    Update poc

--- image/carousel/image-zoom-viewer/App.tsx
+++ image/carousel/image-zoom-viewer/App.tsx
@@ -11,16 +11,27 @@ const Container = styled.View({
 
 const images = [
   { url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460' },
+  {
+    url: 'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/8Ql-wYAJmiMkSg4gvS6zhMewDg4.JPEG',
+  },
   { url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460' },
-  { url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460' },
-  { url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460' },
+  {
+    url: 'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/8Ql-wYAJmiMkSg4gvS6zhMewDg4.JPEG',
+  },
 ];
 
 const App = () => {
   return (
     <Container>
       <Modal visible transparent>
-        <ImageViewer imageUrls={images} />
+        <ImageViewer
+          imageUrls={images}
+          enableImageZoom
+          enableSwipeDown
+          onSwipeDown={() => {
+            console.log('onSwipeDown');
+          }}
+        />
       </Modal>
     </Container>
   );
