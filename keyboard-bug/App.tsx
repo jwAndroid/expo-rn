307b03b3c64bada18069e916095d8092243ae267
jwAndroid <cjd9408er@naver.com>
2022-01-11 12:01:30 +0900

    Update git

--- keyboard-bug/App.tsx
+++ keyboard-bug/App.tsx
@@ -1,20 +1,41 @@
+import { memo, useCallback, useState } from 'react';
+import styled from '@emotion/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const Container = styled.View({
+  flex: 1,
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  backgroundColor: '#303030',
 });
+
+const StyledInput = styled.TextInput({
+  width: '100%',
+  textAlignVertical: 'center',
+  borderColor: '#fff',
+  borderWidth: 1,
+  color: '#000000',
+});
+
+const App = () => {
+  const [textValue, setTextValue] = useState('');
+
+  const onChangeText = useCallback((text: string) => {
+    setTextValue(text.trim());
+  }, []);
+
+  return (
+    <Container>
+      <StatusBar style="auto" />
+
+      <StyledInput
+        value={textValue}
+        placeholder="입력창"
+        onChangeText={onChangeText}
+        multiline
+      />
+    </Container>
+  );
+};
+
+export default memo(App);
