9fca9df91e3e3b211e4e70b6ed696659a920ec73
jwAndroid <cjd9408er@naver.com>
2022-01-13 14:00:26 +0900

    Viewpager update

--- image/carousel/snap-carousel/App.tsx
+++ image/carousel/snap-carousel/App.tsx
@@ -9,10 +9,6 @@ const Container = styled.View({
   flex: 1,
   justifyContent: 'center',
   alignItems: 'center',
-});
-
-const ImageContainer = styled.View({
-  flex: 1,
   backgroundColor: '#303030',
 });
 
@@ -21,40 +17,50 @@ const StyledImage = styled.Image({
 });
 
 const images = [
-  'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko',
-  'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko',
-  'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko',
-  'https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405000019&path-prefix=ko',
+  'https://cdn.notefolio.net/img/d7/5b/d75bf02e2a35f76dba6ed5eeccde793c45d74edd83df838e31290603ceb5c5c9_v1.jpg',
+  'https://t1.daumcdn.net/cfile/tistory/99086B3C5B9B75C431',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSML9Cd9WTepU94wqlWzAGSyKTMXn2Iy1uWAm4cgyjEoseCv50xdo_tIk_fgzN04cd0IqA&usqp=CAU',
+  'https://ai-s2.infcdn.net/screenshots_siandroid/1/16619/16619887_1.jpg',
 ];
 
 const App = () => {
   const renderItem = useCallback(({ item }) => {
-    return (
-      <ImageZoom
-        cropWidth={Dimensions.get('window').width}
-        cropHeight={Dimensions.get('window').height}
-        imageWidth={200}
-        imageHeight={200}
-        enableSwipeDown
-        onSwipeDown={() => {
-          console.log('down');
-        }}
-      >
-        <ImageContainer>
-          <StyledImage source={{ uri: item }} resizeMode="center" />
-        </ImageContainer>
-      </ImageZoom>
-    );
+    return <StyledImage source={{ uri: item }} />;
   }, []);
+
+  // const renderItem = useCallback(({ item }) => {
+  //   return (
+  //     <ImageZoom
+  //       cropWidth={Dimensions.get('window').width}
+  //       cropHeight={Dimensions.get('window').height}
+  //       imageWidth={200}
+  //       imageHeight={200}
+  //       enableSwipeDown
+  //       onSwipeDown={() => {
+  //         console.log('down');
+  //       }}
+  //     >
+  //       <ImageContainer>
+  //         <StyledImage source={{ uri: item }} resizeMode="center" />
+  //       </ImageContainer>
+  //     </ImageZoom>
+  //   );
+  // }, []);
 
   return (
     <Container>
       <StatusBar style="auto" />
 
       <Carousel
-        sliderWidth={200}
-        itemWidth={200}
-        firstItem={1}
+        sliderWidth={400}
+        itemWidth={400}
+        inactiveSlideOpacity={1}
+        inactiveSlideScale={1}
+        autoplay
+        lockScrollWhileSnapping
+        autoplayInterval={5000}
+        loop
+        loopClonesPerSide={3}
         data={images}
         renderItem={renderItem}
       />
