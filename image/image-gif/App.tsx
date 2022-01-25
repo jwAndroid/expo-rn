638ad3b793469e859c4ee331bb70b20f853feebb
jwAndroid <cjd9408er@naver.com>
2022-01-25 17:40:42 +0900

    update npm

--- image/image-gif/App.tsx
+++ image/image-gif/App.tsx
@@ -9,16 +9,22 @@ const Container = styled.View({
   backgroundColor: '#303030',
 });
 
-const GifImage = styled.Image({
-  width: 360,
-  height: 360,
+const Logo = styled.Image({
+  width: '100%',
+  height: 80,
+});
+
+const Divider = styled.View({
+  width: '100%',
+  height: 1,
+  backgroundColor: '#fff',
 });
 
 const App = () => {
   return (
     <Container>
       <TouchableHighlight onPress={() => console.log('click!')}>
-        <GifImage source={require('./assets/test.gif')} />
+        <Logo source={require('./assets/splash-logo.gif')} />
       </TouchableHighlight>
     </Container>
   );
