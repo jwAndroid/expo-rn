a04bf7d0a013a26878a423b42abfaae89423021d
jwAndroid <cjd9408er@naver.com>
2022-01-26 16:22:59 +0900

    Update firebase

---
+++ firebase-react-native/App.tsx
@@ -0,0 +1,21 @@
+import { memo } from 'react';
+import styled from '@emotion/native';
+
+const Container = styled.View({
+  flex: 1,
+});
+
+const StyledText = styled.Text({
+  fontSize: 16,
+  color: '#303030',
+});
+
+const App = () => {
+  return (
+    <Container>
+      <StyledText>hello world</StyledText>
+    </Container>
+  );
+};
+
+export default memo(App);
