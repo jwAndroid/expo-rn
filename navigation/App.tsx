8d971df7c8b976d03a29a025322b41d35c0ab63f
jwAndroid <cjd9408er@naver.com>
2022-02-17 18:35:55 +0900

    update

--- navigation/App.tsx
+++ navigation/App.tsx
@@ -1,14 +1,14 @@
 import React, { memo } from 'react';
 import { StatusBar } from 'expo-status-bar';
 
-import { Navigation } from './src/navigation';
+import { Stack } from './src/navigation';
 
 const App = () => {
   return (
     <>
       <StatusBar style="auto" />
 
-      <Navigation />
+      <Stack />
     </>
   );
 };
