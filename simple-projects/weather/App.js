f2d6dff186385b1f25d375a50d6f3eeefc5c8a5a
jwandroid <cjd9408er@naver.com>
2022-02-26 01:22:21 +0900

    ready for project

---
+++ simple-projects/weather/App.js
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <View style={{ width: 100, height: 100 }}></View>
+      <View style={{ width: 100, height: 100 }}></View>
+      <View style={{ width: 100, height: 100 }}></View>
+    </View>
+  );
+}
