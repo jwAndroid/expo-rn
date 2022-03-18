fb2b1c4f8c61d0ac7e84cef4e53eec8a8ebd293b
jwandroid <cjd9408er@naver.com>
2022-03-18 15:04:05 +0900

    Update SwipeFlatList

--- rn-PoC/flatlist-swipe/App.tsx
+++ rn-PoC/flatlist-swipe/App.tsx
@@ -1,20 +1,165 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { memo, useCallback, useMemo, useState } from 'react';
+import {
+  ListRenderItem,
+  StyleProp,
+  TouchableHighlight,
+  TouchableOpacity,
+  ViewStyle,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import styled from '@emotion/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { ITEM_LIST } from './src/itemList';
+import { Item } from './type';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const StyledText = styled.Text({
+  fontSize: 16,
+  color: '#fff',
 });
+
+const RowBack = styled.View({
+  alignItems: 'center',
+  flex: 1,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  paddingLeft: 5,
+  backgroundColor: '#000000',
+});
+
+const App = () => {
+  const [listData, setListData] = useState<Item[]>(ITEM_LIST);
+
+  const safeAreaContainer = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      flex: 1,
+    }),
+    []
+  );
+
+  const rowFront = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderBottomWidth: 0.5,
+      borderBottomColor: 'black',
+      backgroundColor: 'lightcoral',
+    }),
+    []
+  );
+
+  const closeButton = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      right: 75,
+      backgroundColor: 'blue',
+    }),
+    []
+  );
+
+  const actionButton = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      bottom: 0,
+      position: 'absolute',
+      top: 0,
+      width: 75,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }),
+    []
+  );
+
+  const deleteButton = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      backgroundColor: 'red',
+      right: 0,
+    }),
+    []
+  );
+
+  const closeItem = useCallback(
+    (rowMap, rowKey) => () => {
+      if (rowMap[rowKey]) {
+        rowMap[rowKey].closeRow();
+      }
+    },
+    []
+  );
+
+  const deleteItem = useCallback(
+    (rowMap, rowKey) => () => {
+      closeItem(rowMap, rowKey);
+      const newData = [...listData];
+      const prevIndex = listData.findIndex((item) => item.key === rowKey);
+      newData.splice(prevIndex, 1);
+      setListData(newData);
+    },
+    [closeItem, listData]
+  );
+
+  const onItemOpen = useCallback((rowKey) => {
+    console.log(`Row opened : ${rowKey}`);
+  }, []);
+
+  const onPressItem = useCallback(
+    (item) => () => {
+      console.log(item);
+    },
+    []
+  );
+
+  const renderHiddenItem = useCallback(
+    ({ item }, rowMap) => {
+      return (
+        <RowBack>
+          <TouchableOpacity
+            style={[actionButton, closeButton]}
+            onPress={closeItem(rowMap, item.key)}
+          >
+            <StyledText>Close</StyledText>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[actionButton, deleteButton]}
+            onPress={deleteItem(rowMap, item.key)}
+          >
+            <StyledText>Delete</StyledText>
+          </TouchableOpacity>
+        </RowBack>
+      );
+    },
+    [deleteItem, closeItem, actionButton, deleteButton, closeButton]
+  );
+
+  const renderItem = useCallback<ListRenderItem<Item>>(
+    ({ item }) => {
+      return (
+        <TouchableHighlight onPress={onPressItem(item)} style={rowFront}>
+          <StyledText>
+            텍스트:{item.text} / 키:{item.key}
+          </StyledText>
+        </TouchableHighlight>
+      );
+    },
+    [onPressItem, rowFront]
+  );
+
+  return (
+    <SafeAreaView style={safeAreaContainer}>
+      <SwipeListView
+        data={listData}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        leftOpenValue={0}
+        disableRightSwipe
+        stopRightSwipe={-150}
+        rightOpenValue={-150}
+        previewRowKey="0"
+        previewOpenValue={-40}
+        previewOpenDelay={3000}
+        onRowDidOpen={onItemOpen}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default memo(App);
---
+++ rn-PoC/flatlist-swipe/type.d.ts
@@ -0,0 +1,4 @@
+export type Item = {
+  key: number;
+  text: string;
+};
