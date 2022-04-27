c60c56b4dd2424450cca0906c29f5a738382bbb0
jwandroid <cjd9408er@naver.com>
2022-04-28 03:58:50 +0900

    public-api

--- simple-projects/public-api/App.tsx
+++ simple-projects/public-api/App.tsx
@@ -1,18 +1,67 @@
-import { memo } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import styled from '@emotion/native';
+import {
+  FlatList,
+  ImageSourcePropType,
+  ListRenderItem,
+  Dimensions,
+} from 'react-native';
+import axios from 'axios';
+
+import { FrameEntitiy } from './src/type';
+
+const deviceWidth = Dimensions.get('window').width;
 
 const Container = styled.View({
   flex: 1,
 });
 
-const EmotionsText = styled.Text({
-  fontSize: 16,
+const StyledText = styled.Text({
+  fontSize: 17,
+});
+
+const StyledImage = styled.Image<ImageSourcePropType>({
+  width: deviceWidth,
+  height: 250,
 });
 
 const App = () => {
+  const [data, setData] = useState<FrameEntitiy[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get<FrameEntitiy[]>(
+        'https://picsum.photos/v2/list?page=1'
+      );
+
+      setData(data);
+    })();
+  }, []);
+
+  const keyExtractor = useCallback((item: FrameEntitiy) => `${item.id}`, []);
+
+  const renderItem = useCallback<ListRenderItem<FrameEntitiy>>(({ item }) => {
+    return (
+      <>
+        <StyledImage
+          source={{
+            uri: item.download_url,
+          }}
+          resizeMode="contain"
+        />
+
+        <StyledText>작가:{item.author}</StyledText>
+      </>
+    );
+  }, []);
+
   return (
     <Container>
-      <EmotionsText>hello world</EmotionsText>
+      <FlatList
+        data={data}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+      />
     </Container>
   );
 };
---
+++ simple-projects/public-api/src/type.d.ts
@@ -0,0 +1,5 @@
+export interface FrameEntitiy {
+  id: number;
+  author: string;
+  download_url: string;
+}
