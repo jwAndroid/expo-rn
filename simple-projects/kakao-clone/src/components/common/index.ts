a4d5cb4402c58ecb22846116e127f43f8b4f6d2a
jwandroid <cjd9408er@naver.com>
2022-04-30 08:20:31 +0900

    update, bugfix : list-swipe

--- simple-projects/kakao-clone/src/components/common/index.ts
+++ simple-projects/kakao-clone/src/components/common/index.ts
@@ -1,2 +1,3 @@
-export { default as StyledText } from './StyledText';
 export { default as Header } from './Header';
+export { default as StyledText } from './StyledText';
+export { default as UserCard } from './UserCard';
--- simple-projects/kakao-clone/src/screen/Users.tsx
+++ simple-projects/kakao-clone/src/screen/Users.tsx
@@ -1,11 +1,17 @@
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useMemo, useState } from 'react';
+import {
+  ListRenderItem,
+  StyleProp,
+  TouchableHighlight,
+  TouchableOpacity,
+  ViewStyle,
+} from 'react-native';
 import styled from '@emotion/native';
 import { useTheme } from '@emotion/react';
-import { FlatList, ListRenderItem } from 'react-native';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
-import { Header } from '../components/common';
+import { Header, StyledText, UserCard } from '../components/common';
 import { SafeAreaContainer } from '../components/layout';
-import UserCard from '../components/common/UserCard';
 import { SampleData } from '../api/sample/data';
 import { UserEntity } from '../type';
 
@@ -25,14 +31,124 @@ const ProfileContainer = styled.View(({ theme }) => ({
   backgroundColor: theme.color.gray,
 }));
 
+const RowBack = styled.View({
+  alignItems: 'center',
+  flex: 1,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  paddingLeft: 5,
+  backgroundColor: '#000000',
+});
+
 const Users = () => {
   const theme = useTheme();
-
+  const [listData, setListData] = useState<UserEntity[]>(SampleData);
   const keyExtractor = useCallback((item: UserEntity) => `${item.id}`, []);
 
-  const renderItem = useCallback<ListRenderItem<UserEntity>>(({ item }) => {
-    return <UserCard name={item.name} imageUrl={item.image_url} />;
+  const rowFront = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderBottomWidth: 0.5,
+      borderBottomColor: 'black',
+      backgroundColor: 'lightcoral',
+    }),
+    []
+  );
+
+  const actionButton = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      bottom: 0,
+      position: 'absolute',
+      top: 0,
+      width: 75,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }),
+    []
+  );
+
+  const closeButton = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      right: 75,
+      backgroundColor: 'blue',
+    }),
+    []
+  );
+
+  const deleteButton = useMemo<StyleProp<ViewStyle>>(
+    () => ({
+      backgroundColor: 'red',
+      right: 0,
+    }),
+    []
+  );
+
+  const closeItem = useCallback(
+    (rowMap, rowKey) => () => {
+      if (rowMap[rowKey]) {
+        rowMap[rowKey].closeRow();
+      }
+    },
+    []
+  );
+
+  const deleteItem = useCallback(
+    (rowMap, rowKey) => () => {
+      closeItem(rowMap, rowKey);
+      const newData = [...listData];
+      const prevIndex = listData.findIndex((item) => item.id === rowKey);
+      newData.splice(prevIndex, 1);
+      setListData(newData);
+    },
+    [closeItem, listData]
+  );
+
+  const onItemOpen = useCallback((rowKey) => {
+    console.log(`Row open : ${rowKey}`);
   }, []);
+
+  const onPressItem = useCallback(
+    (item) => () => {
+      console.log(item);
+    },
+    []
+  );
+
+  const renderHiddenItem = useCallback(
+    ({ item }, rowMap) => {
+      return (
+        <RowBack>
+          <TouchableOpacity
+            style={[actionButton, closeButton]}
+            onPress={closeItem(rowMap, item.id)}
+          >
+            <StyledText>Close</StyledText>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[actionButton, deleteButton]}
+            onPress={deleteItem(rowMap, item.id)}
+          >
+            <StyledText>Delete</StyledText>
+          </TouchableOpacity>
+        </RowBack>
+      );
+    },
+    [deleteItem, closeItem, actionButton, deleteButton, closeButton]
+  );
+
+  const renderItem = useCallback<ListRenderItem<UserEntity>>(
+    ({ item }) => {
+      return (
+        <TouchableHighlight onPress={onPressItem(item)} style={rowFront}>
+          <StyledText>텍스트:{item.name}</StyledText>
+        </TouchableHighlight>
+      );
+    },
+    [onPressItem, rowFront]
+  );
 
   return (
     <SafeAreaContainer>
@@ -49,10 +165,19 @@ const Users = () => {
       </ProfileContainer>
 
       <Container>
-        <FlatList
-          data={SampleData}
+        <SwipeListView
+          data={listData}
           keyExtractor={keyExtractor}
           renderItem={renderItem}
+          renderHiddenItem={renderHiddenItem}
+          onRowDidOpen={onItemOpen}
+          leftOpenValue={0}
+          disableRightSwipe
+          stopRightSwipe={-150}
+          rightOpenValue={-150}
+          previewRowKey="0"
+          previewOpenValue={-40}
+          previewOpenDelay={3000}
         />
       </Container>
     </SafeAreaContainer>
