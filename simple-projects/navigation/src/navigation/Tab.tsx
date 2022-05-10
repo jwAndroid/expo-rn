1388288a375eda18c97addae5cb38cb72fea57d5
jwandroid <cjd9408er@naver.com>
2022-05-11 05:24:28 +0900

    update Tab.tsx : hide navigation

--- simple-projects/navigation/src/navigation/Tab.tsx
+++ simple-projects/navigation/src/navigation/Tab.tsx
@@ -3,13 +3,14 @@ import {
   BottomTabNavigationOptions,
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 
 import { MailStack, ProfileStack, SettingsStack } from '../screen';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 const Tab = () => {
-  const bottomNavigationOption = useMemo<BottomTabNavigationOptions>(
+  const screenOptions = useMemo<BottomTabNavigationOptions>(
     () => ({
       headerShown: false,
       tabBarLabelPosition: 'beside-icon',
@@ -26,41 +27,66 @@ const Tab = () => {
     []
   );
 
-  const mailOptions = useMemo<BottomTabNavigationOptions>(
-    () => ({
-      tabBarLabel: '메일',
-    }),
-    []
-  );
-
-  const prifleOptions = useMemo<BottomTabNavigationOptions>(
-    () => ({
-      tabBarLabel: '프로필',
-    }),
-    []
-  );
-
-  const settingsOptions = useMemo<BottomTabNavigationOptions>(
-    () => ({
-      tabBarLabel: '세팅',
-    }),
-    []
-  );
-
   return (
-    <Navigator screenOptions={bottomNavigationOption}>
-      <Screen name="MailStack" component={MailStack} options={mailOptions} />
+    <Navigator screenOptions={screenOptions}>
+      <Screen
+        name="MailStack"
+        component={MailStack}
+        options={({ route }) => {
+          const routeName = getFocusedRouteNameFromRoute(route) ?? 'Mail';
+
+          return {
+            tabBarStyle: {
+              display: routeName === 'Mail' ? 'flex' : 'none',
+              height: 60,
+              backgroundColor: '#303030',
+            },
+            tabBarIconStyle: {
+              display: 'none',
+            },
+            tabBarLabel: '홈',
+          };
+        }}
+      />
 
       <Screen
         name="ProfileStack"
         component={ProfileStack}
-        options={prifleOptions}
+        options={({ route }) => {
+          const routeName = getFocusedRouteNameFromRoute(route) ?? 'Profile';
+
+          return {
+            tabBarStyle: {
+              display: routeName === 'Profile' ? 'flex' : 'none',
+              height: 60,
+              backgroundColor: '#303030',
+            },
+            tabBarIconStyle: {
+              display: 'none',
+            },
+            tabBarLabel: '프로필',
+          };
+        }}
       />
 
       <Screen
         name="SettingsStack"
         component={SettingsStack}
-        options={settingsOptions}
+        options={({ route }) => {
+          const routeName = getFocusedRouteNameFromRoute(route) ?? 'Settings';
+
+          return {
+            tabBarStyle: {
+              display: routeName === 'Settings' ? 'flex' : 'none',
+              height: 60,
+              backgroundColor: '#303030',
+            },
+            tabBarIconStyle: {
+              display: 'none',
+            },
+            tabBarLabel: '세팅',
+          };
+        }}
       />
     </Navigator>
   );
