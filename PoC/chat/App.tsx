c34b2667595a3f1a68aa3300eef4db5bb41ebf5e
jwandroid <cjd9408er@naver.com>
2022-06-22 18:52:32 +0900

    update day header

--- PoC/chat/App.tsx
+++ PoC/chat/App.tsx
@@ -1,7 +1,81 @@
-import { memo } from 'react';
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
+import {
+  FlatList,
+  ListRenderItem,
+  StyleProp,
+  Text,
+  View,
+  ViewStyle,
+} from 'react-native';
+
+import { sampleData } from './src/sampledata';
+import { ChatEntity } from './src/type';
 
 const App = () => {
-  return <></>;
+  const [data, setData] = useState<ChatEntity[]>(sampleData);
+
+  useEffect(() => {
+    const list1 = [...data];
+    const list2 = [...data];
+
+    const deduplication = list1.filter(
+      (arr, index, callback) =>
+        index === callback.findIndex((t) => t.date === arr.date)
+    );
+
+    const dateDeleteList = list2.map((data) => {
+      return { ...data, date: '' };
+    });
+
+    const result = [...deduplication, ...dateDeleteList].filter(
+      (arr, index, callback) =>
+        index === callback.findIndex((t) => t.id === arr.id)
+    );
+
+    const sorted = result.sort((a, b) => a.id - b.id);
+
+    setData(sorted);
+  }, [data]);
+
+  const ListHeaderComponentStyle = useMemo<StyleProp<ViewStyle>>(() => {
+    return {
+      backgroundColor: 'blue',
+    };
+  }, []);
+
+  const keyExtractor = useCallback((item) => `${item.id}`, []);
+
+  const ListHeaderComponent = useCallback((item) => {
+    return (
+      <View>
+        <Text>{item.date}</Text>
+      </View>
+    );
+  }, []);
+
+  const renderItem = useCallback<ListRenderItem<ChatEntity>>(({ item }) => {
+    return (
+      <View style={{ alignItems: 'flex-end' }}>
+        {item.date === '' ? undefined : (
+          <Text style={{ marginTop: 10 }}>{item.date}</Text>
+        )}
+
+        <Text style={{ fontSize: 13, color: 'blue' }}>{item.message}</Text>
+      </View>
+    );
+  }, []);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: 'gray' }}>
+      <FlatList
+        data={data}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
+        ListHeaderComponentStyle={ListHeaderComponentStyle}
+      />
+    </View>
+  );
 };
 
 export default memo(App);
--- PoC/chat/src/sampledata.ts
+++ PoC/chat/src/sampledata.ts
@@ -45,3 +45,18 @@ export const giftedSampleData = [
     },
   },
 ];
+
+export const sampleData = [
+  { id: 1, message: '메세지1', date: '2022-06-22' },
+  { id: 2, message: '메세지2', date: '2022-06-22' },
+  { id: 3, message: '메세지3', date: '2022-06-22' },
+  { id: 4, message: '메세지4', date: '2022-06-23' },
+  { id: 5, message: '메세지5', date: '2022-06-24' },
+  { id: 6, message: '메세지6', date: '2022-06-24' },
+  { id: 7, message: '메세지7', date: '2022-06-25' },
+  { id: 8, message: '메세지8', date: '2022-06-25' },
+  { id: 9, message: '메세지9', date: '2022-06-25' },
+  { id: 10, message: '메세지10', date: '2022-06-26' },
+  { id: 11, message: '메세지11', date: '2022-06-25' },
+  { id: 12, message: '메세지12', date: '2022-06-27' },
+];
