5c552ed060522f4559c638b2aef80d9e9412338a
jwandroid <cjd9408er@naver.com>
2022-06-28 07:52:44 +0900

    Prog: link-preview-PoC

--- library/linkPreview/App.tsx
+++ library/linkPreview/App.tsx
@@ -1,28 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, BackHandler } from 'react-native';
+import { View } from 'react-native';
 import { LinkPreview } from '@flyerhq/react-native-link-preview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LinkPreview text="https://docs.expo.dev" />
-
-      <Text
-        onPress={() => {
-          console.log('asd');
-        }}
-      >
-        adasd
-      </Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <LinkPreview text="http://json.parser.online.fr/" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ library/rn-lodash/src/data.ts
@@ -0,0 +1,7 @@
+export const data = [
+  { BUILDING: '01', BUILDING_NAME: '1호관', ROOM_NO: '1-001B', ROOM: '1-001B' },
+  { BUILDING: '01', BUILDING_NAME: '1호관', ROOM_NO: '1-002', ROOM: '1-002' },
+  { BUILDING: '01', BUILDING_NAME: '1호관', ROOM_NO: '1-011A', ROOM: '1-011A' },
+  { BUILDING: '01', BUILDING_NAME: '1호관', ROOM_NO: '1-011B', ROOM: '1-011B' },
+  // ...
+];
