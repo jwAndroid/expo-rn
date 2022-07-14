d558d04dab06618fba2ec6bcc32a3d007bed3026
jwAndroid <cjd9408er@naver.com>
2022-07-14 12:38:08 +0900

    Update: expo

--- library/linkPreview/App.tsx
+++ library/linkPreview/App.tsx
@@ -4,7 +4,7 @@ import { LinkPreview } from '@flyerhq/react-native-link-preview';
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <LinkPreview text="http://json.parser.online.fr/" />
+      <LinkPreview text="https://developer.android.com/jetpack" />
     </View>
   );
 }
