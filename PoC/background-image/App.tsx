91f2a7d49277b0a688b85e6fa5ba2cffcbf59354
jwAndroid <cjd9408er@naver.com>
2022-11-16 17:50:25 +0900

    Add Project background-image

---
+++ PoC/background-image/App.tsx
@@ -0,0 +1,12 @@
+import { memo } from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default memo(App);
